f425fbe263acaaaadf3a9fccf48f30cc5e542724
JPShee <jeremypsheehy@gmail.com>
2017-03-10 13:05:12 -0400

    Added username or email login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def login=(login)
+  	@login = login
+  end
+
+  def login
+  	@login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+    	where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  	else
+    	where(conditions).first
+  	end
+  end
 end
