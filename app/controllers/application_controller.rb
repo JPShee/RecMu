d1d2c1d759e3fd6a9f2d91fd516656cff120ceba
JPShee <jeremypsheehy@gmail.com>
2017-03-13 14:51:41 -0300

    Added defaulikes string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,4 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
 		devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me)}
 	end
-
-
 end
---
+++ db/migrate/20170313165033_add_likes_to_user.rb
@@ -0,0 +1,8 @@
+#This looks gross but I got tired of trying to concatenate the strings
+#There are 114 zeroes in the string
+class AddLikesToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :likes, :string, :default => "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
+  end
+end
